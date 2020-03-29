24062fc48ebce4f4d30928aad45c8f4d8fbca5c3
Tejvir90 <47678473+Tejvir90@users.noreply.github.com>
2020-03-29 02:35:11 -0400

    new project for TDD analysis

---
+++ springtests/src/test/java/com/tdd/springtests/SpringtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.springtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
